0293270e9b920e9983133bd6280dd46fa2941f44
Omar Adel <omaradelezz54@gmail.com>
2022-10-29 00:54:50 +0200

    added on click event for window

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-$(".navigation__link").click(function(o){document.querySelector(".navigation__checkbox").checked=!1}),"#popup"===location.hash&&(window.onclick=function(o){"popup"===o.target.className&&(location.hash="section-tours")});
+$(".navigation__link").click(function (e) {
+  document.querySelector(".navigation__checkbox").checked = false;
+});
+
+window.onclick = function (event) {
+  if (location.hash === "#popup" && event.target.className === "popup") {
+    location.hash = "#section-tours";
+  }
+};
